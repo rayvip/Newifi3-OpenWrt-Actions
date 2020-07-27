9a0dad2c7a9da83015b9d67135ceb369478e7354
gbaoye <gbaoye@hotmail.com>
2020-07-27 18:13:49 +0800

    Update OpenWrt-Lienol.sh

--- OpenWrt-Lienol.sh
+++ OpenWrt-Lienol.sh
@@ -83,6 +83,12 @@ cat >> .config <<EOF
 CONFIG_TARGET_ramips=y
 CONFIG_TARGET_ramips_mt7621=y
 CONFIG_TARGET_ramips_mt7621_DEVICE_d-team_newifi-d2=y
+CONFIG_HAS_SUBTARGETS=y
+CONFIG_HAS_DEVICES=y
+CONFIG_TARGET_BOARD="ramips"
+CONFIG_TARGET_SUBTARGET="mt7621"
+CONFIG_TARGET_PROFILE="DEVICE_d-team_newifi-d2"
+CONFIG_TARGET_ARCH_PACKAGES="mipsel_24kc"
 EOF
 
 # 设置固件大小:
@@ -101,19 +107,32 @@ EOF
 #CONFIG_EFI_IMAGES=y
 #EOF
 
+# 无线驱动
+cat >> .config <<EOF
+CONFIG_PACKAGE_wireless-regdb=y
+CONFIG_PACKAGE_kmod-cfg80211=y
+CONFIG_PACKAGE_kmod-mac80211=y
+CONFIG_PACKAGE_MAC80211_DEBUGFS=y
+CONFIG_PACKAGE_MAC80211_MESH=y
+CONFIG_PACKAGE_kmod-7615d=y
+CONFIG_PACKAGE_luci-app-mtwifi=y
+EOF
+
 # IPv6支持:
 cat >> .config <<EOF
 CONFIG_IPV6=y
+CONFIG_PACKAGE_kmod-ipt-nat6=y
+CONFIG_PACKAGE_kmod-ip6tables-extra=y
+CONFIG_PACKAGE_kmod-nf-nat6=y
+CONFIG_PACKAGE_6in4=y
 CONFIG_PACKAGE_dnsmasq_full_dhcpv6=y
+CONFIG_PACKAGE_ip6tables=y
 CONFIG_PACKAGE_ipv6helper=y
 CONFIG_PACKAGE_odhcp6c=y
+CONFIG_PACKAGE_odhcp6c_ext_cer_id=0
 CONFIG_PACKAGE_odhcpd-ipv6only=y
+CONFIG_PACKAGE_odhcpd_ipv6only_ext_cer_id=0
 CONFIG_PACKAGE_luci-proto-ipv6=y
-CONFIG_PACKAGE_kmod-ipt-nat6=y
-CONFIG_PACKAGE_ipv6helper=y
-CONFIG_PACKAGE_kmod-ip6tables=y
-CONFIG_PACKAGE_kmod-ip6tables-extra=y
-CONFIG_PACKAGE_6in4=y
 EOF
 
 # 多文件系统支持:
