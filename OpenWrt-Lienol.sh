7cee72711629acf7844f3b99ab9ead0e65a92e19
gbaoye <gbaoye@hotmail.com>
2020-08-22 04:15:25 +0800

    Update OpenWrt-Lienol.sh

--- OpenWrt-Lienol.sh
+++ OpenWrt-Lienol.sh
@@ -273,11 +273,11 @@ EOF
 
 # LuCI主题:
 cat >> .config <<EOF
-CONFIG_PACKAGE_luci-theme-atmaterial=y
-CONFIG_PACKAGE_luci-theme-argon-dark-mod=n
-CONFIG_PACKAGE_luci-theme-argon-light-mod=n
+CONFIG_PACKAGE_luci-theme-atmaterial=n
+CONFIG_PACKAGE_luci-theme-argon-dark-mod=y
+CONFIG_PACKAGE_luci-theme-argon-light-mod=y
 CONFIG_PACKAGE_luci-theme-bootstrap=y
-CONFIG_PACKAGE_luci-theme-edge=n
+CONFIG_PACKAGE_luci-theme-edge=y
 # CONFIG_PACKAGE_luci-theme-bootstrap-mod is not set
 # CONFIG_PACKAGE_luci-theme-darkmatter is not set
 # CONFIG_PACKAGE_luci-theme-freifunk-generic is not set
