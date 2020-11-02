f3058094f84a2d789c7d7d11ee4b759475e715e0
gbaoye <gbaoye@hotmail.com>
2020-11-02 17:13:23 +0800

    Update OpenWrt-Lean.sh

--- OpenWrt-Lean.sh
+++ OpenWrt-Lean.sh
@@ -16,10 +16,11 @@ cat feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
 
 # 添加第三方软件包
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 
 # 替换更新插件
 #rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
