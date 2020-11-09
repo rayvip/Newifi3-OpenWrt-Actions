c4605a6b7a7368ba7fd077584290c398656e77a3
gbaoye <gbaoye@hotmail.com>
2020-11-09 09:05:24 +0800

    Update OpenWrt-Lienol.sh

--- OpenWrt-Lienol.sh
+++ OpenWrt-Lienol.sh
@@ -20,7 +20,7 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 #git clone https://github.com/xiaorouji/openwrt-package package/openwrt-packages
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
