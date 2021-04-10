7aecb53ae6f61fc90612cfa726db4c9458474ba3
gbaoye <gbaoye@hotmail.com>
2021-04-10 19:32:32 +0800

    Update OpenWrt-KFERMercer.sh

--- OpenWrt-KFERMercer.sh
+++ OpenWrt-KFERMercer.sh
@@ -20,12 +20,12 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 #git clone https://github.com/xiaorouji/openwrt-package package/openwrt-packages
-#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
-git clone https://github.com/SuLingGG/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/peter-tank/luci-app-clash package/luci-app-clash
+#git clone https://github.com/SuLingGG/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/peter-tank/luci-app-clash package/luci-app-clash
 
 # 替换更新插件
 #rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
