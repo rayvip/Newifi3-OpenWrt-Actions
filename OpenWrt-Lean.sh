ab0800c53d0a454f1bdc3e371acbbf077e5b0edf
gbaoye <gbaoye@hotmail.com>
2021-04-11 15:31:16 +0800

    Update OpenWrt-Lean.sh

--- OpenWrt-Lean.sh
+++ OpenWrt-Lean.sh
@@ -20,13 +20,13 @@ cat feeds.conf.default
 
 # 添加第三方软件包
 #git clone https://github.com/xiaorouji/openwrt-package package/openwrt-packages
-git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
+#git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
-#git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-#git clone https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash package/luci-app-openclash
-#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-openclash package/luci-app-openclash
 
 # 替换更新插件
 #rm -rf package/openwrt-packages/luci-app-passwall && svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-app-passwall package/openwrt-packages/luci-app-passwall
