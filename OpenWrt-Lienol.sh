46ea7b92d9fa8cd1be35d468680e405fdd91ede2
gbaoye <gbaoye@hotmail.com>
2021-04-15 09:57:25 +0800

    Update OpenWrt-Lienol.sh

--- OpenWrt-Lienol.sh
+++ OpenWrt-Lienol.sh
@@ -29,11 +29,11 @@ rm -rf ./package/diy/luci-app-adguardhome
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-#git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
 #git clone https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-openclash package/luci-app-openclash
 
 # 下载自定义软件
-svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-clash ../package/luci-app-clash
+#svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-clash ../package/luci-app-clash
 svn co https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-openclash ../package/luci-app-openclash
 
 # 替换更新插件
@@ -165,7 +165,7 @@ EOF
 # 第三方插件选择:
 cat >> .config <<EOF
 #CONFIG_PACKAGE_luci-app-oaf=y #应用过滤
-#CONFIG_PACKAGE_luci-app-clash=y
+CONFIG_PACKAGE_luci-app-clash=y
 CONFIG_PACKAGE_luci-app-openclash=y #OpenClash客户端
 #CONFIG_PACKAGE_luci-app-serverchan=y #微信推送
 #CONFIG_PACKAGE_luci-app-eqos=y #IP限速
